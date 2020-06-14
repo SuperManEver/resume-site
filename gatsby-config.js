5d5d0b23c99f41574048faba1b904f6f5f6227d5
SuperManEver <nikita.luparev@gmail.com>
2020-06-14 13:31:23 +0500

    few tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nick's resume`,
+    description: "Nick's persone website",
+    author: `nikita.luparev@gmail.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
